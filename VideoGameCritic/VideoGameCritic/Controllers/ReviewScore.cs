e6db28c40dbcaf1f7e8723b44a6399262fe0fe50
sLill <SamuelTurnerLill@gmail.com>
2023-09-15 22:06:56 -0500

    -

---
+++ VideoGameCritic/VideoGameCritic/Controllers/ReviewScore.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VideoGameCritic.Data;
+
+namespace VideoGameCritic.Controllers
+{
+    public class ReviewScore : Controller
+    {
+        #region Fields..
+        private readonly IGamesRepository _gamesRepository;
+        #endregion Fields..
+
+        #region Constructors..
+        public ReviewScore(IGamesRepository gamesRepository)
+        {
+            _gamesRepository = gamesRepository;
+        }
+        #endregion Constructors..
+
+        #region Methods..
+        // GET: ReviewScore
+        public ActionResult Index()
+        {
+            return View();
+        } 
+        #endregion Methods..
+    }
+}
