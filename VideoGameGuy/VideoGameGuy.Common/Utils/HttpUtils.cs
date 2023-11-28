3dc0c7cf8dbd367f95ecaff19bf351f24ed21e34
sLill <SamuelTurnerLill@gmail.com>
2023-11-28 01:22:46 -0600

    -

---
+++ VideoGameGuy/VideoGameGuy.Common/Utils/HttpUtils.cs
@@ -0,0 +1,31 @@
+﻿namespace VideoGameGuy.Common
+{
+    public static class HttpUtils
+    {
+        #region Methods..
+        public static bool UrlExists(string url)
+        {
+            bool result = false;
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Head, url);
+                    var response = client.Send(request);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        
+                    }
+
+                    return response.IsSuccessStatusCode;
+                }
+                catch (HttpRequestException) { }
+            }
+
+            return result;
+        }
+        #endregion Methods..
+    }
+}
